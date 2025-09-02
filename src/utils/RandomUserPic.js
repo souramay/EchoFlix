ea656aaeff05fa1660b51fc7420fff7b2526c16a
Souramay Bhowmik <130826449+souramay@users.noreply.github.com>
2025-09-03 01:57:24 +0530

    Implement code changes to enhance functionality and improve performance

--- src/utils/RandomUserPic.js
+++ src/utils/RandomUserPic.js
@@ -1,6 +1,6 @@
 const RandomUserPic = () => {
   // returns /useprofile/1.png .. /useprofile/4.png
-  return "/userProfile/" + (Math.floor(Math.random() * 4) + 1) + ".png";
+  return "/userProfile/" + (Math.floor(Math.random() * 5) + 1) + ".png";
 };
 
 export default RandomUserPic;
