64cd2ed22e5e1aad9a08558ea60b157f6138fc43
Souramay Bhowmik <130826449+souramay@users.noreply.github.com>
2025-09-17 11:52:24 +0530

    Implement code changes to enhance functionality and improve performance

--- src/utils/constants/ImgConst.js
+++ src/utils/constants/ImgConst.js
@@ -1,8 +1,12 @@
 // image path and urls
 export const logo = "/img/logo/logo2.png";
-export const backgroundImg = "/img/Background/Background2.jpg";
+export const backgroundImg = getRandomBgImg();
 export const logoTab = "/img/logo/logo-tab2.png";
 export const footerImg="/userProfile/Admin/Adobe Express - file.png"
+export function getRandomBgImg() {
+	const num = Math.floor(Math.random() * 3) + 1; // 1 to 3
+	return `/img/Background/Background${num}.jpg`;
+}
 
 
 
